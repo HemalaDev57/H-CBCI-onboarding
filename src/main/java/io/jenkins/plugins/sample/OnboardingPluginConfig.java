baa3763caf02b4b65193b0cd7746e38f75b99b66
HemalaDev57 <hdevanan@cloudbees.com>
2026-02-09 01:44:10 +0530

    Level 2 commit

--- src/main/java/io/jenkins/plugins/sample/OnboardingPluginConfig.java
+++ src/main/java/io/jenkins/plugins/sample/OnboardingPluginConfig.java
@@ -2,8 +2,13 @@ package io.jenkins.plugins.sample;
 
 
 import org.kohsuke.stapler.DataBoundSetter;
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
+
+import net.sf.json.JSONObject;
 
 import hudson.Extension;
+import hudson.util.FormValidation;
 import jenkins.model.GlobalConfiguration;
 
 @Extension
@@ -23,8 +28,10 @@ public class OnboardingPluginConfig extends GlobalConfiguration {
 
     @DataBoundSetter
     public void setName(String name) {
-        this.name = name;
-        save();
+        if (name.matches("^[a-zA-Z ]+$")) {
+            this.name = name;
+            save();
+        }
     }
 
     public String getDescription() {
@@ -35,6 +42,17 @@ public class OnboardingPluginConfig extends GlobalConfiguration {
     public void setDescription(String description) {
         this.description = description;
         save();
+    }
+
+    public FormValidation doCheckName(@QueryParameter String value) {
+        if (value.length() == 0) {
+            return FormValidation.error("Please set the name");
+        }
+        if (!value.matches("^[a-zA-Z ]+$")) {
+            return FormValidation.error("Invalid format. The name should only contain lowercase, uppercase letters and "
+                                        + "spaces");
+        }
+        return FormValidation.ok();
     }
 
 }
