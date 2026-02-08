93922c02f539305d2e446988d1a956b7f47fc97b
HemalaDev57 <hdevanan@cloudbees.com>
2026-02-09 02:12:04 +0530

    Level 3 commit

--- src/main/java/io/jenkins/plugins/sample/OnboardingPluginConfig.java
+++ src/main/java/io/jenkins/plugins/sample/OnboardingPluginConfig.java
@@ -8,7 +8,15 @@ import org.kohsuke.stapler.StaplerRequest;
 import net.sf.json.JSONObject;
 
 import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.model.Build;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.Builder;
 import hudson.util.FormValidation;
+import hudson.util.Secret;
 import jenkins.model.GlobalConfiguration;
 
 @Extension
@@ -17,6 +25,12 @@ public class OnboardingPluginConfig extends GlobalConfiguration {
     private String name;
 
     private String description;
+
+    private boolean connectionConfig;
+
+    private String userName;
+
+    private Secret password;
 
     public OnboardingPluginConfig() {
         load();
@@ -32,6 +46,36 @@ public class OnboardingPluginConfig extends GlobalConfiguration {
             this.name = name;
             save();
         }
+    }
+
+    public boolean getConnectionConfig() {
+        return connectionConfig;
+    }
+
+    @DataBoundSetter
+    public void setConnectionConfig(boolean connectionConfig) {
+        this.connectionConfig = connectionConfig;
+        save();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    @DataBoundSetter
+    public void setUserName(String userName) {
+        this.userName = userName;
+        save();
+    }
+
+    public Secret getPassword() {
+        return password;
+    }
+
+    @DataBoundSetter
+    public void setPassword(Secret password) {
+        this.password = password;
+        save();
     }
 
     public String getDescription() {
@@ -53,6 +97,30 @@ public class OnboardingPluginConfig extends GlobalConfiguration {
                                         + "spaces");
         }
         return FormValidation.ok();
+    }
+
+    @Override
+    public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
+        String submittedName = json.getString("name");
+        if (!submittedName.matches("^[a-zA-Z ]+$")) {
+            throw new FormException("Invalid name. Data not saved.", "name");
+        }
+        this.name = submittedName;
+        this.description = json.getString("description");
+
+        if (json.containsKey("connectionConfig")) {
+            JSONObject block = json.getJSONObject("connectionConfig");
+            this.connectionConfig = true;
+            this.userName = block.getString("userName");
+            // Converting plain text string to Secret object
+            this.password = Secret.fromString(block.getString("password"));
+        } else {
+            this.connectionConfig = false;
+            this.userName = null;
+            this.password = null;
+        }
+        save();
+        return true;
     }
 
 }
